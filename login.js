d66384776fb051c03c7f1462e02ac36c6836f012
jpharriman <johnwpaulharriman@gmail.com>
2018-06-15 21:24:13 -0400

    login

--- login.js
+++ login.js
@@ -2,41 +2,62 @@
 
 $(function (){
 
-	alert("work");
-
-
 	var $username = "jpharriman"
 	var $password = "2muchSwag"
 
-	$.ajax({
-		type: 'POST',
-		url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
-		// beforeSend: function (xhr){
-		// 	xhr.setRequestHeader('Authorization','Basic ')
+	//$.ajax({
+		// type: 'POST',
+		// url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
+		// // beforeSend: function (xhr){
+		// // 	xhr.setRequestHeader('Authorization','Basic ')
+		// // }
+		// dataType: 'jsonp',
+		// data: {
+		// 	username: 'jpharriman',
+		// 	password: '2muchSwag'
+		// },
+		// crossDomain: true,
+		// xhrFields: {
+		// 	withCredentials: true
+		// },
+		// success : function(data) {
+		// 	alert("logged in");
+		// },
+		// error: function (xhr, ajaxOptions, throwError){
+		// 	alert("wrong");
 		// }
-		dataType: 'jsonp',
-		data: {
-			username: 'jpharriman',
-			password: '2muchSwag'
-		},
-		crossDomain: true,
-		xhrFields: {
-			withCredentials: true
-		},
-		success : function(data) {
-			alert("logged in");
-		},
-		error: function (xhr, ajaxOptions, throwError){
-			alert("wrong");
-		}
+		// datatype: 'jsonp',
 
-	})
+		// xhrFields: {
+		// 	withCredentials: true
+		// },
+		// headers: {
+		// 	'Authorization': 'Basic ' + btoa('jpharriman:2muchSwag')
+		// },
+		// url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
+		// success : function(data) {
+		// 	alert("logged in");
+		// },
+		// error: function (xhr, ajaxOptions, throwError){
+		// 	alert("wrong");
+		// }
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "https//:gpodder.net/api/2/auth/jpharriman/login.json", true);
+		xhr.withCredentials = true;
+		xhr.setRequestHeader("Authorization", 'Basic ' + btoa('jpharriman:2muchSwag'));
+		xhr.onload = function () {
+		    console.log(xhr.responseText);
+		};
+		xhr.send();
 
-	alert($username.val);
+
+
+	//})
+
 
 	$('#submitBtn').on("click",function(event){
 		event.preventDefault();
-		alert("working");
+		//alert("working");
 	});
 
 
