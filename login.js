0d06f586017c571ecb25b97ee285a753edb63253
jpharriman <johnwpaulharriman@gmail.com>
2018-06-16 05:31:20 -0400

    login attempt

--- login.js
+++ login.js
@@ -6,59 +6,21 @@ $(function (){
 	var $password = "2muchSwag"
 
 	$.ajax({
-		// type: 'GET',
-		// url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
-		// // beforeSend: function (xhr){
-		// // 	xhr.setRequestHeader('Authorization','Basic ')
-		// // }
-		// dataType: 'jsonp',
-		// // data: {
-		// // 	username: 'jpharriman',
-		// // 	password: '2muchSwag'
-		// // },
-		// //crossDomain: true,
-		// xhrFields: {
-		// 	withCredentials: true
-		// },
-		// beforeSend: function (xhr) {
-		// 	xhr.setRequestHeader("Authorization", 'Basic ' + btoa('jpharriman:2muchSwag'));
-		// },
-		// success : function(data) {
-		// 	alert("logged in");
-		// },
-		// error: function (xhr, ajaxOptions, throwError){
-		// 	alert("wron");
-		// }
-		type: 'GET',
-		url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
-		data: {"test":true},
-		dataType: 'jsonp',
-		async: "false",
-		xhrFields: {
-			withCredentials: true
+
+		type: "GET",
+		url: "",
+		dataType: 'json',
+		async: false,
+		headers: {
+			"Authorization": "Basic " + btoa($username + ":" + $password)
 		},
-		success: function(data) {
-			alert("WORKS");
+		data: '{comment}',
+		success: function (){
+			alert("thanks");
 		},
-		beforeSend: function(xhr) {
-			xhr.setRequestHeader('Authorization', 'Basic ' + btoa('jpharriman:2muchSwag'));
+		error: function (){
+			alert("no thanks");
 		}
-		// datatype: 'jsonp',
-
-		// xhrFields: {
-		// 	withCredentials: true
-		// },
-		// headers: {
-		// 	'Authorization': 'Basic ' + btoa('jpharriman:2muchSwag')
-		// },
-		// url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
-		// success : function(data) {
-		// 	alert("logged in");
-		// },
-		// error: function (xhr, ajaxOptions, throwError){
-		// 	alert("wrong");
-		// }
-
 
 
 	});
@@ -74,35 +36,3 @@ $(function (){
 
 });
 
-// $(function (){
-// 	var $username = $('#inputUsername');
-// 	var $password = $('#inputPassword');
-
-// 	alert("con");
-
-// 	$('#submitBtn').on("submit",function(event){
-// 		event.preventDefault();
-// 		alert("yes");
-
-// 		var req = {
-// 				username = $username.val(),
-// 				password = '2muchSwag',
-// 			};
-
-// 		$.ajax({
-// 			type: 'POST',
-// 			url: 'https://gpodder.net/api/2/auth/jpharriman/login.json',
-// 			data: req,
-// 			success: function(resp) {
-// 				alert("yes");
-
-// 			},
-// 			error: function(){
-// 				alert("no");
-// 			}
-
-// 		});
-
-// 	});
-	
-// });
